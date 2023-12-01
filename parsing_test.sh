f342e5ec17513bc434db07eebb5e0a3867d27c6a
Zaiqiang Wu <wuzaiqiang@zju.edu.cn>
2023-12-02 00:14:04 +0900

    updated

--- parsing_test.sh
+++ parsing_test.sh
@@ -1,6 +1,4 @@
 # Example of inference
-python exp/inference/inference.py  \
---loadmodel ./universal_trained.pth \
---img_path ./ichao_input.jpg \
---output_path ./img/ \
---output_name /output_file_name.jpg
+#python exp/inference/inference.py  --loadmodel ./universal_trained.pth --img_path ./ichao_input.jpg --output_path ./img/ --output_name /output_file_name.jpg
+python exp/inference/inference.py  --loadmodel ./cihp2pascal.pth --img_path ./ichao_input.jpg --output_path ./img/ --output_name /output_file_name.jpg
+#python exp/inference/inference.py  --loadmodel ./inference.pth --img_path ./ichao_input.jpg --output_path ./img/ --output_name /output_file_name.jpg
